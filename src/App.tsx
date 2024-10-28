551504b08948394b17730cebb8e9b4b69681dd1d
Omar Baldi <omar.baldi.business@gmail.com>
2024-10-28 19:52:18 +0100

    Applied debounced logic on uncontrolled input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+/* eslint-disable */
 import "./App.css";
 
+function debounce<T>(fn: (...args: T[]) => void, delay: number) {
+  let timeout: ReturnType<typeof setTimeout>;
+
+  return (e: Parameters<typeof fn>[0]) => {
+    clearTimeout(timeout);
+    const fnToExecute = () => fn(e);
+    timeout = setTimeout(fnToExecute, delay);
+  };
+}
+
 function App() {
-  return <></>;
+  function extractPeriodicElements(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value: updatedInputV } = e.target;
+    //TODO: apply logic here
+  }
+
+  const debounceInputChange = debounce(extractPeriodicElements, 500);
+
+  return (
+    <>
+      <input type="text" onChange={debounceInputChange} />
+    </>
+  );
 }
 
 export default App;
